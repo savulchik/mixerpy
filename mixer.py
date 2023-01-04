35de51881795b72ce88a63676f0287960db1de2c
Stanislav Savulchik <s.savulchik@gmail.com>
2023-01-04 21:18:25 +0700

    Calculate amplitude RMS

--- mixer.py
+++ mixer.py
@@ -1,19 +1,18 @@
 import sounddevice as sd
-import queue
 import numpy as np
 import time
 import rrdtool
 import os
+import logging
+import sys
 
-
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO, stream=sys.stdout)
 samplerate = 44100
 block_duration_seconds = 1
+blocksize = int(samplerate * block_duration_seconds)
 rrd_file = 'mixer.rrd'
-
-blocks = queue.SimpleQueue()
-
-def callback(indata: np.ndarray, frames: int, time, status: sd.CallbackFlags) -> None:
-	blocks.put_nowait(indata)
+# bins = 16
+# window = np.hamming(bins)
 
 if not os.path.exists(rrd_file):
 	rrdtool.create(
@@ -24,11 +23,22 @@ if not os.path.exists(rrd_file):
 
 with sd.InputStream(
 	samplerate=samplerate, 
-	blocksize=int(samplerate * block_duration_seconds),
+	dtype=np.int16,
 	channels=1,
-	callback=callback) as stream:
+	latency='low') as stream:
 	while True:
-		block = blocks.get()
-		max_aplitude = np.amax(block)
-		print(f'Max amplitude: {max_aplitude}')
-		rrdtool.update(rrd_file, f'N:{max_aplitude}')
+		block, _ = stream.read(frames=blocksize)
+		start_ns = time.monotonic_ns()
+		block = block.reshape(-1)
+		rms_amplitude = np.sqrt(np.mean(np.square(block)))
+		end_ns = time.monotonic_ns()
+		elapsed_ns = end_ns - start_ns
+		logging.info(f'RMS: {rms_amplitude:.2f}, elapsed ms: {int(elapsed_ns / 1_000_000)}')
+		# block = block * window
+		# magnitudes = np.abs(np.fft.rfft(block, n=bins))
+		# frequencies = np.fft.rfftfreq(n=bins, d=1./samplerate)
+
+		# print(magnitudes, frequencies, sep='\n')
+		# max_aplitude = np.amax(block)
+		# print(f'Max amplitude: {max_aplitude}')
+		# rrdtool.update(rrd_file, f'N:{max_aplitude}')
