4a9cdabc827b19271ceadc77c03f719869d3879d
Stanislav Savulchik <s.savulchik@gmail.com>
2023-01-04 22:45:55 +0700

    Update rrd file

--- mixer.py
+++ mixer.py
@@ -10,15 +10,15 @@ logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO, stream
 samplerate = 44100
 block_duration_seconds = 1
 blocksize = int(samplerate * block_duration_seconds)
-rrd_file = 'mixer.rrd'
-# bins = 16
-# window = np.hamming(bins)
+rrd_dir = os.path.dirname(os.path.abspath(__file__))
+rrd_file = os.path.join(rrd_dir, 'mixer.rrd')
+logging.info(f'Writing to {rrd_file}')
 
 if not os.path.exists(rrd_file):
 	rrdtool.create(
 		rrd_file, 
 		'--step', '1s',
-		'DS:amplitude:GAUGE:1m:0:1', 
+		'DS:amplitude:GAUGE:1m:0:32767',
 		'RRA:AVERAGE:0.5:1s:1day')
 
 with sd.InputStream(
@@ -33,12 +33,5 @@ with sd.InputStream(
 		rms_amplitude = np.sqrt(np.mean(np.square(block)))
 		end_ns = time.monotonic_ns()
 		elapsed_ns = end_ns - start_ns
+		rrdtool.update(rrd_file, f'N:{rms_amplitude}')
 		logging.info(f'RMS: {rms_amplitude:.2f}, elapsed ms: {int(elapsed_ns / 1_000_000)}')
-		# block = block * window
-		# magnitudes = np.abs(np.fft.rfft(block, n=bins))
-		# frequencies = np.fft.rfftfreq(n=bins, d=1./samplerate)
-
-		# print(magnitudes, frequencies, sep='\n')
-		# max_aplitude = np.amax(block)
-		# print(f'Max amplitude: {max_aplitude}')
-		# rrdtool.update(rrd_file, f'N:{max_aplitude}')
