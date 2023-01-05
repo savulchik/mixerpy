79322792af847bc2f856fd846e7be62fb9f2203b
Stanislav Savulchik <s.savulchik@gmail.com>
2023-01-05 22:48:41 +0700

    Use typer for mixer.py

--- mixer.py
+++ mixer.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import sounddevice as sd
 import numpy as np
 import time
@@ -5,33 +7,54 @@ import rrdtool
 import os
 import logging
 import sys
+import typer
+from typing import Optional, Union
 
-logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO, stream=sys.stdout)
-samplerate = 44100
-block_duration_seconds = 1
-blocksize = int(samplerate * block_duration_seconds)
-rrd_dir = os.path.dirname(os.path.abspath(__file__))
-rrd_file = os.path.join(rrd_dir, 'mixer.rrd')
-logging.info(f'Writing to {rrd_file}')
+app = typer.Typer()
 
-if not os.path.exists(rrd_file):
-	rrdtool.create(
-		rrd_file, 
-		'--step', '1s',
-		'DS:amplitude:GAUGE:1m:0:32767',
-		'RRA:AVERAGE:0.5:1s:1day')
+def get_input_device_info(device_index: Optional[int]) -> dict:
+    return sd.query_devices(device_index, kind='input') if device_index is not None else sd.query_devices(kind='input')
 
-with sd.InputStream(
-	samplerate=samplerate, 
-	dtype=np.int16,
-	channels=1,
-	latency='low') as stream:
-	while True:
-		block, _ = stream.read(frames=blocksize)
-		start_ns = time.monotonic_ns()
-		block = block.reshape(-1)
-		rms_amplitude = np.sqrt(np.mean(np.square(block)))
-		end_ns = time.monotonic_ns()
-		elapsed_ns = end_ns - start_ns
-		rrdtool.update(rrd_file, f'N:{rms_amplitude}')
-		logging.info(f'RMS: {rms_amplitude:.2f}, elapsed ms: {int(elapsed_ns / 1_000_000)}')
+@app.command()
+def list_devices():
+    print(sd.query_devices())
+    return
+
+
+@app.command()
+def record(device_index: Optional[int] = None):
+    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO, stream=sys.stdout)
+    samplerate = 44100
+    block_duration_seconds = 1
+    blocksize = int(samplerate * block_duration_seconds)
+    rrd_dir = os.path.dirname(os.path.abspath(__file__))
+    rrd_file = os.path.join(rrd_dir, 'mixer.rrd')
+    logging.info(f'Writing to {rrd_file}')
+    logging.info(f'Using {get_input_device_info(device_index)} for recording')
+
+    if not os.path.exists(rrd_file):
+        rrdtool.create(
+            rrd_file,
+            '--step', '1s',
+            'DS:amplitude:GAUGE:1m:0:32767',
+            'RRA:AVERAGE:0.5:1s:1day')
+
+    with sd.InputStream(
+            samplerate=samplerate,
+            device=device_index,
+            dtype=np.int16,
+            channels=1,
+            latency='low') as stream:
+        while True:
+            block, _ = stream.read(frames=blocksize)
+            start_ns = time.monotonic_ns()
+            block = block.reshape(-1)
+            rms_amplitude = np.sqrt(np.mean(np.square(block)))
+            end_ns = time.monotonic_ns()
+            elapsed_ns = end_ns - start_ns
+            rrdtool.update(rrd_file, f'N:{rms_amplitude}')
+            logging.info(f'RMS: {rms_amplitude:.2f}, elapsed ms: {int(elapsed_ns / 1_000_000)}')
+
+
+if __name__ == "__main__":
+    app()
